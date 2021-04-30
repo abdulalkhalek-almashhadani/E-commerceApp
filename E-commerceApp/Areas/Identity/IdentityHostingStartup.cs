e0180e2bf2fa527d88f3cfd2599b240b441596ee
ALKHALEQ ALMASHADANI <ALKHALEQ.ALMASHADANI.0320@abchorizon.com>
2021-04-30 18:41:47 +0300

    sys

---
+++ E-commerceApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EcommerceApp1.Areas.Identity.Data;
+using EcommerceApp1.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EcommerceApp1.Areas.Identity.IdentityHostingStartup))]
+namespace EcommerceApp1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EcommerceApp1Context>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EcommerceApp1ContextConnection")));
+
+                services.AddDefaultIdentity<EcommerceApp1User>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<EcommerceApp1Context>();
+            });
+        }
+    }
+}
---
+++ E-commerceApp/Models/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceApp1.Models
+{
+    public class BaseEntity <T>
+    {
+        public T Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime LastModify { get; set; }
+
+        public bool IsDelete { get; set; }
+    }
+}
