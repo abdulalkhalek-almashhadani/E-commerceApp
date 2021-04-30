e38ad6b0378a15be927aca644bcf9da3656c3b62
ALKHALEQ ALMASHADANI <ALKHALEQ.ALMASHADANI.0320@abchorizon.com>
2021-04-30 20:56:52 +0300

    Revert "Add .gitignore and .gitattributes."
    This reverts commit 44581ed7777177ed9b8abf8cf88b2b122834b679.

---
+++ E-commerceApp/Models/Brands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceApp1.Models
+{
+    public class Brands:BaseEntity <int>
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string ImageId { get; set; }
+    }
+}
